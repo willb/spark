f2ef54e4ec92d8f0ee3e91af4f507bcabd29a7c0
William Benton <willb@redhat.com>
2014-03-13 14:21:45 -0500

    Generalized proactive closure serialization test.

--- core/src/test/scala/org/apache/spark/serializer/ProactiveClosureSerializationSuite.scala
+++ core/src/test/scala/org/apache/spark/serializer/ProactiveClosureSerializationSuite.scala
@@ -21,36 +21,59 @@ import java.io.NotSerializableException
 
 import org.scalatest.FunSuite
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkException
 import org.apache.spark.SharedSparkContext
 
+/* a trivial (but unserializable) container for trivial functions */
 class UnserializableClass {
-    def op[T](x: T) = x.toString
+  def op[T](x: T) = x.toString
+  
+  def pred[T](x: T) = x.toString.length % 2 == 0
 }
-
-class SerializableClass extends UnserializableClass with Serializable { }
 
 class ProactiveClosureSerializationSuite extends FunSuite with SharedSparkContext {
 
-    test("throws expected serialization exceptions on actions") {
-        val data = sc.parallelize(0 until 1000)
-        val uc = new UnserializableClass
-    
-        val ex = intercept[SparkException] {
-            data.map(uc.op(_)).count
-        }
-        
-        assert(ex.getCause.isInstanceOf[NotSerializableException])
-    }
+  def fixture = (sc.parallelize(0 until 1000).map(_.toString), new UnserializableClass)
 
-    test("throws proactive serialization exceptions on transformations") {
-        val data = sc.parallelize(0 until 1000)
-        val uc = new UnserializableClass
-    
-        val ex = intercept[SparkException] {
-            data.map(uc.op(_))
-        }
-        
-        assert(ex.getCause.isInstanceOf[NotSerializableException])
+  test("throws expected serialization exceptions on actions") {
+    val (data, uc) = fixture
+      
+    val ex = intercept[SparkException] {
+      data.map(uc.op(_)).count
     }
+        
+    assert(ex.getMessage.matches(".*Task not serializable.*"))
+  }
+
+  // There is probably a cleaner way to eliminate boilerplate here, but we're
+  // iterating over a map from transformation names to functions that perform that
+  // transformation on a given RDD, creating one test case for each
+  
+  for (transformation <- 
+      Map("map" -> xmap _, "flatMap" -> xflatMap _, "filter" -> xfilter _, "mapWith" -> xmapWith _,
+          "mapPartitions" -> xmapPartitions _, "mapPartitionsWithIndex" -> xmapPartitionsWithIndex _,
+          "mapPartitionsWithContext" -> xmapPartitionsWithContext _, "filterWith" -> xfilterWith _)) {
+    val (name, xf) = transformation
+    
+    test(s"$name transformations throw proactive serialization exceptions") {
+      val (data, uc) = fixture
+      
+      val ex = intercept[SparkException] {
+        xf(data, uc)
+      }
+
+      assert(ex.getMessage.matches(".*Task not serializable.*"), s"RDD.$name doesn't proactively throw NotSerializableException")
+    }
+  }
+  
+  private def xmap(x: RDD[String], uc: UnserializableClass): RDD[String] = x.map(y=>uc.op(y))
+  private def xmapWith(x: RDD[String], uc: UnserializableClass): RDD[String] = x.mapWith(x => x.toString)((x,y)=>x + uc.op(y))
+  private def xflatMap(x: RDD[String], uc: UnserializableClass): RDD[String] = x.flatMap(y=>Seq(uc.op(y)))
+  private def xfilter(x: RDD[String], uc: UnserializableClass): RDD[String] = x.filter(y=>uc.pred(y))
+  private def xfilterWith(x: RDD[String], uc: UnserializableClass): RDD[String] = x.filterWith(x => x.toString)((x,y)=>uc.pred(y))
+  private def xmapPartitions(x: RDD[String], uc: UnserializableClass): RDD[String] = x.mapPartitions(_.map(y=>uc.op(y)))
+  private def xmapPartitionsWithIndex(x: RDD[String], uc: UnserializableClass): RDD[String] = x.mapPartitionsWithIndex((_, it) => it.map(y=>uc.op(y)))
+  private def xmapPartitionsWithContext(x: RDD[String], uc: UnserializableClass): RDD[String] = x.mapPartitionsWithContext((_, it) => it.map(y=>uc.op(y)))
+  
 }
