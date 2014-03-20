5ae37dc9d014a74adcd7c1244c5f3f71a4495940
William Benton <willb@redhat.com>
2014-03-20 10:48:17 -0500

    Predictable closure environment capture
    The environments of serializable closures are now captured as
    part of closure cleaning. Since we already proactively check most
    closures for serializability, ClosureCleaner.clean now returns
    the result of deserializing the serialized version of the cleaned
    closure.

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -1140,7 +1140,7 @@ class SparkContext(config: SparkConf) extends Logging {
    * Clean a closure to make it ready to serialized and send to tasks
    * (removes unreferenced variables in $outer's, updates REPL variables)
    */
-  private[spark] def clean[F <: AnyRef](f: F): F = {
+  private[spark] def clean[F <: AnyRef : ClassTag](f: F): F = {
     clean(f, true)
   }
 
@@ -1148,9 +1148,8 @@ class SparkContext(config: SparkConf) extends Logging {
    * Clean a closure to make it ready to serialized and send to tasks
    * (removes unreferenced variables in $outer's, updates REPL variables)
    */
-  private[spark] def clean[F <: AnyRef](f: F, checkSerializable: Boolean): F = {
+  private[spark] def clean[F <: AnyRef : ClassTag](f: F, checkSerializable: Boolean): F = {
     ClosureCleaner.clean(f, checkSerializable)
-    f
   }
 
   /**
