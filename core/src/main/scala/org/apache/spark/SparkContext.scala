070dce73ec6a824fafd7383eeef0589668522ed1
William Benton <willb@redhat.com>
2014-03-25 23:45:45 -0500

    Skip proactive closure capture for runJob
    There are two possible cases for runJob calls: either they are called
    by RDD action methods from inside Spark or they are called from client
    code. There's no need to proactively check the closure argument to
    runJob for serializability or force variable capture in either case:
    
    1. if they are called by RDD actions, their closure arguments consist
    of mapping an already-serializable closure (with an already-frozen
    environment) to each element in the RDD;
    
    2. in both cases, the closure is about to execute and thus the benefit
    of proactively checking for serializability (or ensuring immediate
    variable capture) is nonexistent.
    
    (Note that ensuring capture via serializability on closure arguments to
    runJob also causes pyspark accumulators to fail to update.)

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -1033,7 +1033,9 @@ class SparkContext(config: SparkConf) extends Logging {
       throw new SparkException("SparkContext has been shutdown")
     }
     val callSite = getCallSite
-    val cleanedFunc = clean(func)
+    // There's no need to check this function for serializability,
+    // since it will be run right away.
+    val cleanedFunc = clean(func, false)
     logInfo("Starting job: " + callSite)
     val start = System.nanoTime
     dagScheduler.runJob(rdd, cleanedFunc, partitions, callSite, allowLocal,
